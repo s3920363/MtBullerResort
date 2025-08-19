b450de1a34de86b9c7a9aa993b9f47f6b5dd589e
Ricky Nguyen <s3920363@student.rmit.edu.au>
2025-08-19 10:21:48 +1000

    add Customer class with attributes and methods for customer management

---
+++ src/Customer.java
@@ -0,0 +1,52 @@
+public class Customer {
+    private static int nextId = 1;
+    private int custId;
+    private String name;
+    private String email;
+    private String skillLevel;
+
+    public Customer() {
+        this.custId = nextId++;
+    }
+
+    public Customer(String name, String email, String skillLevel) {
+        this.custId = nextId++;
+        this.name = name;
+        this.email = email;
+        this.skillLevel = skillLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(String skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    @Override
+    public String toString() {
+        // Construct and return a string that describes the customer.
+        return String.format(
+                "{ id: %d, name: \"%s\", email: \"%s\", skillLevel: \"%s\" }",
+                custId, name, email, skillLevel
+        );
+    }
+
+}
