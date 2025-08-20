1b460ddf99a162afa72f000e793c5aa24ddd7509
Ricky Nguyen <s3920363@student.rmit.edu.au>
2025-08-20 23:37:46 +1000

    refactor MtBullerAdmin class; consolidate package management and accommodation listing into a single run method

--- src/MtBullerAdmin.java
+++ src/MtBullerAdmin.java
@@ -2,8 +2,6 @@ public class MtBullerAdmin {
     public static void main(String[] args) {
         MtBullerResort mt = new MtBullerResort();
         mt.populateLists();
-        mt.addPackage();
-        mt.listAllAccommodations();
-        mt.addPackage();
+        mt.run();
     }
 }
