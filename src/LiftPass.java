524871912bb22570e0546f470c7b4acf7e827f44
Ricky Nguyen <s3920363@student.rmit.edu.au>
2025-08-28 02:32:01 +1000

    add LiftPass class with cost calculation and string representation

---
+++ src/LiftPass.java
@@ -0,0 +1,41 @@
+public class LiftPass {
+    private String type;
+    private int days;
+    private double cost;
+
+    public LiftPass() {
+
+    }
+
+    public LiftPass(String type, int days) {
+        this.type = type;
+        this.days = days;
+        calculateCost();
+    }
+
+    public void calculateCost() {
+        if (type.equalsIgnoreCase("Season")) {
+            cost = 200;
+        } else if (type.equalsIgnoreCase("Daily")) {
+            cost = 26 * days;
+            if (days == 5) {
+                cost = cost * 0.9; // 10% discount
+            }
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        if (type.equalsIgnoreCase("Season")) {
+            return "Lift Pass: Season ($200)";
+        } else if (type.equalsIgnoreCase("Daily")) {
+            return "Lift Pass: " + days + " days ($" + cost + ")";
+        } else {
+            return "Lift Pass: None";
+        }
+    }
+}
